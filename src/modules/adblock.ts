5795f2b90d01f807a1c525811a3ca823bc733935
hkmikemak <hkmikemak@gmail.com>
2020-07-03 15:24:57 +0800

    Adopt rxjs

--- src/modules/adblock.ts
+++ src/modules/adblock.ts
@@ -1,35 +1,54 @@
-import chalk from "chalk";
 import { downloadText } from "./downloadHelper";
+import { from, Observable, of } from 'rxjs';
+import { filter, flatMap, map, mergeAll } from 'rxjs/operators';
 
 const REGEX_DOMAIN = new RegExp("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$");
 const REGEX_IPV4 = new RegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
 const REGEX_IPV6 = new RegExp("^(?:(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){6})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:::(?:(?:(?:[0-9a-fA-F]{1,4})):){5})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){4})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,1}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){3})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,2}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){2})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,3}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:[0-9a-fA-F]{1,4})):)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,4}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,5}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,6}(?:(?:[0-9a-fA-F]{1,4})))?::))))$");
 
-const processLines = (lines: string): string[] => {
-  return lines
-    .split(/\r?\n/) // Split by linebreak
-    .map((i) => (i.includes("#") ? i.substring(0, i.indexOf("#")) : i)) // For each line, remove trailing comments
-    .reduce((agg, i) => agg.concat(i.split(/\s+/)), []) // For each line, split by whitespace
-    .filter((i) => i.length > 0)
-    .filter((i) => !REGEX_IPV6.test(i))
-    .filter((i) => !REGEX_IPV4.test(i))
-    .filter((i) => REGEX_DOMAIN.test(i));
-};
+const processLines = (lines: string): Observable<string> =>
+  of(lines).pipe(
+    // Split whole text file into line by line array, then flatten the array
+    map(i => i.split(/\r?\n/)),
+    mergeAll(),
+
+    // Remove any text after "#"
+    map(i => (i.includes("#") ? i.substring(0, i.indexOf("#")) : i)),
+
+    // Splie each line further into chunk by whitespaces, then  flatten the array agaain
+    map(i => i.split(/\s+/)),
+    mergeAll(),
+
+    // Filter each chunk, only accept result if it is a actual domain name
+    filter(i => i.length > 0),
+    filter(i => !REGEX_IPV6.test(i)),
+    filter(i => !REGEX_IPV4.test(i)),
+    filter(i => REGEX_DOMAIN.test(i)),
+  );
 
 const generateHosts: (adblockSources: string[], domainWhitelist: string[], domainBlacklist: string[]) => Promise<string[]> = async (adblockSources, domainWhitelist, domainBlacklist) => {
-  const result = new Set<string>(domainBlacklist);
+  return new Promise<string[]>((resolve, reject) => {
 
-  for (const url of adblockSources) {
-    try {
-      console.log(`Downloading ${url}`);
-      const lines = await downloadText(url);
-      processLines(lines).filter((i) => !domainWhitelist.some((w) => i.endsWith(w))).forEach((i) => { result.add(i); });
-      console.log(chalk.green(`Success - ${result.size} domain(s) so far`));
-    } catch (err) {
-      console.log(chalk.red(`Failed - ${err.code} - ${err.message}`));
-    }
-  }
-  return [...result].sort();
+    const result = new Set<string>(domainBlacklist);
+    const urls = new Set<string>(adblockSources);
+
+    from(urls).pipe(
+      // swicth map to another observable
+      flatMap(url => from(downloadText(url))),
+      // Convert text to text Array
+      flatMap(i => processLines(i)),
+      // Filter against whitelist
+      filter(i => !domainWhitelist.some(w => i.endsWith(w))),
+    ).subscribe(
+      (next) => {
+        result.add(next);
+      },
+      null,
+      () => {
+        resolve([...result].sort());
+      }
+    );
+  });
 };
 
 export { generateHosts };
--- src/modules/downloadHelper.ts
+++ src/modules/downloadHelper.ts
@@ -8,45 +8,58 @@ import path from 'path';
 import * as tmp from 'tmp';
 
 const downloadText = async (url: string): Promise<string> => {
-  if (url.toLocaleLowerCase().endsWith("7z")) {
-    const tmpDir: string = tmp.dirSync().name;
-    const tmpFile: string = tmp.tmpNameSync();
+  try {
+    if (url.toLocaleLowerCase().endsWith("7z")) {
+      const tmpDir: string = tmp.dirSync().name;
+      const tmpFile: string = tmp.tmpNameSync();
 
-    // Download file
-    const response = await axios.get(url, { responseType: "stream" });
+      // Download file
+      console.log(chalk.yellow(`Begin download ${url}`));
+      const response = await axios.get(url, { responseType: "stream" });
+      console.log(chalk.green(`Download ${url} sucessfully`));
 
-    // Save file to disk
-    await new Promise<void>((resolve, reject) => {
-      const stream = fs.createWriteStream(tmpFile);
-      response.data.pipe(stream);
-      stream.on("finish", resolve);
-      stream.on("err", reject);
-    });
-
-    await new Promise<void>((resolve, reject) => {
-      exec(`${path7za} x "${tmpFile}" -o"${tmpDir}" -r`, (err, stdout, strerr) => {
-        if (err) {
-          console.log(chalk.red(`Failed to extract - ${err.message}`));
-          console.log(chalk.red(`Failed to extract - ${stdout}`));
-          console.log(chalk.red(`Failed to extract - ${strerr}`));
-          reject(err);
-        }
-        resolve();
+      // Save file to disk
+      console.log(chalk.yellow(`Begin extract ${url}`));
+      await new Promise<void>((resolve, reject) => {
+        const stream = fs.createWriteStream(tmpFile);
+        response.data.pipe(stream);
+        stream.on("finish", resolve);
+        stream.on("err", reject);
       });
-    });
 
-    // Read Files
-    const files = fs.readdirSync(tmpDir);
-    const result: string = files.map((i) => path.resolve(tmpDir, i)).map((i) => fs.readFileSync(i)).reduce((aggr, i) => aggr + "\r\n" + i, "");
+      await new Promise<void>((resolve, reject) => {
+        exec(`${path7za} x "${tmpFile}" -o"${tmpDir}" -r`, (err, stdout, strerr) => {
+          if (err) {
+            console.log(chalk.red(`Failed to extract - ${err.message}`));
+            console.log(chalk.red(`Failed to extract - ${stdout}`));
+            console.log(chalk.red(`Failed to extract - ${strerr}`));
+            reject(err);
+          }
+          resolve();
+        });
+      });
 
-    // Cleanup
-    del.sync([tmpFile], { force: true });
-    del.sync([tmpDir], { force: true });
+      // Read Files
+      const files = fs.readdirSync(tmpDir);
+      const result: string = files.map((i) => path.resolve(tmpDir, i)).map((i) => fs.readFileSync(i)).reduce((aggr, i) => aggr + "\r\n" + i, "");
 
-    return result;
-  } else {
-    const response = await axios.get(url, { responseType: "blob" });
-    return response.data as string;
+      // Cleanup
+      del.sync([tmpFile], { force: true });
+      del.sync([tmpDir], { force: true });
+
+      console.log(chalk.green(`Extract ${url} sucessfully - Result size: ${result.length}`));
+
+      return result;
+    } else {
+      console.log(chalk.yellow(`Begin download ${url}`));
+      const response = await axios.get(url, { responseType: "blob" });
+      const result: string = response.data as string;
+      console.log(chalk.green(`Download ${url} sucessfully - Result size: ${result.length}`));
+      return result;
+    }
+  } catch{
+    console.log(chalk.red(`Failed to procerss ${url}`));
+    return '';
   }
 };
 
